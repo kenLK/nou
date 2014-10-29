b07dd6818c4a91fd9e45f083e758d8616f875223
Ken <Ken1@hiflydemacbook.local>
2014-10-30 00:52:27 +0800

    Initial Commit

---
+++ nou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nou
+//
+//  Created by Ken on 2014/10/30.
+//  Copyright (c) 2014年 Ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nou
+//
+//  Created by Ken on 2014/10/30.
+//  Copyright (c) 2014年 Ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
