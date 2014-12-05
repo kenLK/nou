0cf7ff43b4b66324081f451d9166d4a7577b4253
focusardi <focusardi@focusardideMacBook-Pro.local>
2014-12-05 11:04:38 +0800

    test

--- nou/AppDelegate.m
+++ nou/AppDelegate.m
@@ -22,7 +22,7 @@
     // Override point for customization after application launch.
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    
+     
     // Override point for customization after application launch.
     
     self.window.backgroundColor = [UIColor whiteColor];
