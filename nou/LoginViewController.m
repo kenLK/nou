ffa6873b19973675322ef2cdcf5dfd44e720698f
focusardi <focusardi@focusardideMacBook-Pro.local>
2014-12-11 11:07:22 +0800

    bounds applcationFrame height fix

--- nou/LoginViewController.m
+++ nou/LoginViewController.m
@@ -28,38 +28,42 @@
     // Do any additional setup after loading the view from its nib.
     
     
+    CGRect appRect = [[UIScreen mainScreen] applicationFrame];
     CGRect screenRect = [[UIScreen mainScreen] bounds];
     
-    CGFloat screenWidth = screenRect.size.width;
-    CGFloat screenHeight = screenRect.size.height;//screenRect.size.height;
-    NSLog(@"screenWidth>>>%f", screenWidth);
-    NSLog(@"screenHeight>>>%f", screenHeight);
     
-    CGFloat yWidth = screenWidth / 1200.0;
-    CGFloat yHeight = screenHeight / 1920.0;
+    CGFloat modHeight = screenRect.size.height - appRect.size.height;
+    
+    CGFloat appWidth = appRect.size.width;
+    CGFloat appHeight = appRect.size.height;//screenRect.size.height;
+    NSLog(@"appWidth>>>%f", appWidth);
+    NSLog(@"appHeight>>>%f", appHeight);
+    
+    CGFloat yWidth = appWidth / 1200.0;
+    CGFloat yHeight = appHeight / 1920.0;
 
     //背景
-    UIImageView *backImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0 , 0.0, yWidth*1200, yHeight*1920)];
+    UIImageView *backImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0 , 0.0 + modHeight, yWidth*1200, yHeight*1920)];
     UIImage *backImage = [UIImage imageWithContentsOfFile:
                       [[NSBundle mainBundle] pathForResource:@"bg_V" ofType:@"jpg"]];
     [backImageView setImage:backImage];
     [self.view addSubview:backImageView];
     
     //logo
-    UIImageView *logoImageView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*395, yHeight*1787, yWidth*410, yHeight*90)];
+    UIImageView *logoImageView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*395, yHeight*1787 + modHeight, yWidth*410, yHeight*90)];
     UIImage *logoImage = [UIImage imageWithContentsOfFile:
                           [[NSBundle mainBundle] pathForResource:@"icon_logo" ofType:@"png"]];
     [logoImageView setImage:logoImage];
     [self.view addSubview:logoImageView];
     
     //功能title
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, yWidth*1200, yHeight*174)];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0 + modHeight, yWidth*1200, yHeight*174)];
     UIImage *image = [UIImage imageWithContentsOfFile:
                       [[NSBundle mainBundle] pathForResource:@"alpha_header_bg" ofType:@"png"]];
     [imageView setImage:image];
     [self.view addSubview:imageView];
     
-    UILabel* functionTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.0, yWidth*1200, yHeight*174)];
+    UILabel* functionTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.0 + modHeight, yWidth*1200, yHeight*174)];
     [functionTitleLabel setText:@"國立空中大學"];
     [functionTitleLabel setFont:[UIFont fontWithName:@"微軟正黑體" size:yHeight*60]];
     functionTitleLabel.textColor = [UIColor whiteColor];
@@ -67,7 +71,7 @@
     [self.view addSubview:functionTitleLabel];
     
     //標題
-    UILabel* titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(yWidth*78, yHeight*290, yWidth*1044, yHeight*142)];
+    UILabel* titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(yWidth*78, yHeight*290 + modHeight, yWidth*1044, yHeight*142)];
     [titleLabel setText:@"行動化服務系統"];
     [titleLabel setFont:[UIFont fontWithName:@"微軟正黑體" size:yHeight*60]];
     titleLabel.backgroundColor = [self colorFromHexString:@"34aadc"];
@@ -76,34 +80,34 @@
     [self.view addSubview:titleLabel];
     
     //欄位底色
-    UIImageView *formBgView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*78, yHeight*432, yWidth*1044, yHeight*600)];
+    UIImageView *formBgView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*78, yHeight*432 + modHeight, yWidth*1044, yHeight*600)];
     UIImage *formBgImage = [UIImage imageWithContentsOfFile:
                       [[NSBundle mainBundle] pathForResource:@"alpha_white_bg" ofType:@"png"]];
     [formBgView setImage:formBgImage];
     [self.view addSubview:formBgView];
     
     //帳號欄位底色
-    UIImageView *accountBgView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*178, yHeight*504, yWidth*844, yHeight*100)];
+    UIImageView *accountBgView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*178, yHeight*504 + modHeight, yWidth*844, yHeight*100)];
     UIImage *accountBgImage = [UIImage imageWithContentsOfFile:
                             [[NSBundle mainBundle] pathForResource:@"alpha_box_bg" ofType:@"png"]];
     [accountBgView setImage:accountBgImage];
     [self.view addSubview:accountBgView];
     
     //帳號icon
-    UIImageView *accountIconView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*202, yHeight*514, yWidth*80, yHeight*80)];
+    UIImageView *accountIconView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*202, yHeight*514 + modHeight, yWidth*80, yHeight*80)];
     UIImage *accountIconImage = [UIImage imageWithContentsOfFile:
                                [[NSBundle mainBundle] pathForResource:@"icon_account" ofType:@"png"]];
     [accountIconView setImage:accountIconImage];
     [self.view addSubview:accountIconView];
     
     //帳號欄位
-    account = [[UITextField alloc]initWithFrame:CGRectMake(yWidth*312, yHeight*504, yWidth*710, yHeight*100)];
+    account = [[UITextField alloc]initWithFrame:CGRectMake(yWidth*312, yHeight*504 + modHeight, yWidth*710, yHeight*100)];
     account.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"請輸入帳號" attributes:@{NSForegroundColorAttributeName: [self colorFromHexString:@"000000"], NSFontAttributeName:[UIFont fontWithName:@"微軟正黑體" size:yHeight*50]}];
     [self.view addSubview:account];
     
     //帳號清除欄位資訊按鈕
     UIButton *clearAccountButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    clearAccountButton.frame = CGRectMake(yWidth*942, yHeight*514, yWidth*80, yHeight*80);
+    clearAccountButton.frame = CGRectMake(yWidth*942, yHeight*514 + modHeight, yWidth*80, yHeight*80);
     [clearAccountButton addTarget:self action:@selector(clearAccount:) forControlEvents:UIControlEventTouchUpInside];
     UIImage *clearAccountImage = [UIImage imageWithContentsOfFile:
                                   [[NSBundle mainBundle] pathForResource:@"icon_clear" ofType:@"png"]];
@@ -114,27 +118,27 @@
     [self.view addSubview:clearAccountButton];
     
     //密碼欄位底色
-    UIImageView *passwordBgView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*178, yHeight*634, yWidth*844, yHeight*100)];
+    UIImageView *passwordBgView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*178, yHeight*634 + modHeight, yWidth*844, yHeight*100)];
     UIImage *passwordBgImage = [UIImage imageWithContentsOfFile:
                                [[NSBundle mainBundle] pathForResource:@"alpha_box_bg" ofType:@"png"]];
     [passwordBgView setImage:passwordBgImage];
     [self.view addSubview:passwordBgView];
     
     //密碼icon
-    UIImageView *passwordIconView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*202, yHeight*644, yWidth*80, yHeight*80)];
+    UIImageView *passwordIconView = [[UIImageView alloc] initWithFrame:CGRectMake(yWidth*202, yHeight*644 + modHeight, yWidth*80, yHeight*80)];
     UIImage *passwordIconImage = [UIImage imageWithContentsOfFile:
                                  [[NSBundle mainBundle] pathForResource:@"icon_password" ofType:@"png"]];
     [passwordIconView setImage:passwordIconImage];
     [self.view addSubview:passwordIconView];
     
     //密碼欄位
-    password = [[UITextField alloc]initWithFrame:CGRectMake(yWidth*312, yHeight*634, yWidth*710, yHeight*100)];
+    password = [[UITextField alloc]initWithFrame:CGRectMake(yWidth*312, yHeight*634 + modHeight, yWidth*710, yHeight*100)];
     password.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"請輸入密碼" attributes:@{NSForegroundColorAttributeName: [self colorFromHexString:@"000000"], NSFontAttributeName:[UIFont fontWithName:@"微軟正黑體" size:yHeight*50]}];
     [self.view addSubview:password];
     
     //帳號清除欄位資訊按鈕
     UIButton *clearPasswordButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    clearPasswordButton.frame = CGRectMake(yWidth*942, yHeight*644, yWidth*80, yHeight*80);
+    clearPasswordButton.frame = CGRectMake(yWidth*942, yHeight*644 + modHeight, yWidth*80, yHeight*80);
     [clearPasswordButton addTarget:self action:@selector(clearPassword:) forControlEvents:UIControlEventTouchUpInside];
     UIImage *clearPasswordImage = [UIImage imageWithContentsOfFile:
                                   [[NSBundle mainBundle] pathForResource:@"icon_clear" ofType:@"png"]];
@@ -152,7 +156,7 @@
     [loginButton setTitle:@"登入" forState:UIControlStateNormal];
     [loginButton setTitleColor:[self colorFromHexString:@"ffffff"] forState:UIControlStateNormal];
     loginButton.backgroundColor = [self colorFromHexString:@"f27935"];
-    loginButton.frame = CGRectMake(yWidth*178, yHeight*842, yWidth*844, yHeight*100);
+    loginButton.frame = CGRectMake(yWidth*178, yHeight*842 + modHeight, yWidth*844, yHeight*100);
     [self.view addSubview:loginButton];
     
 }
